985bf70b9f30c48d38dacbb96c1ac2b8b16865f7
Christopher <c.freden@gmail.com>
2013-11-10 13:28:28 +0100

    Added export to window.kitsune

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-module.exports = {
-  router: require('./lib/router'),
-  view: require('./lib/view'),
-  xhr: require('./lib/xhr'),
-  log: require('./lib/log')
-};
+var kitsune = {
+      router: require('./lib/router'),
+      view: require('./lib/view'),
+      xhr: require('./lib/xhr'),
+      log: require('./lib/log')
+    },
+    originalKitsune;
+
+if (typeof window !== 'undefined') {
+  originalKitsune = window.kitsune;
+  kitsune.noConflict = function() {
+    window.kitsune = originalKitsune;
+    return kitsune;
+  };
+  window.kitsune = kitsune;
+}
+
+module.exports = kitsune;
